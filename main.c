e4409854bac7e03a8d8423530f3fe9a38b4ea166
Luis Diaz <99diazluisfernand@gmail.com>
2019-08-25 18:11:33 -0500

    Segunda Funcion Terminada

--- main.c
+++ main.c
@@ -11,12 +11,15 @@ Matricula: 1821936
 int main() 
 { 
  /* Actividad 1*/
-	float x, resultado;  /* se declara el resultado y la variable "x" como tipo flotante en caso de que se encuentre un entero con decimal como respuesta de la funcion*/ 
+	float x, resultado, resultadoDos;  /* se declara el resultado y la variable "x" como tipo flotante en caso de que se encuentre un entero con decimal como respuesta de la funcion*/ 
+							 /* se agregan las variables necesarias para los 3 resultados*/
 	printf("indique el valor de x para las funciones \n");  /* se le indica al us teclear el valor de x para la funcion mencionada*/
 	scanf("%f", &x); 		 
 	resultado=(x-1)/4-(x-5)/36;   /* se le asigna el valor de la funcion a la variable correspondiente*/
+	resultadoDos = 6*((x+1)/8-(2*x-3)/16);		/* se le asigna el valor de la funcion a la variable correspondiente*/
 	
 	printf("el valor de la primera funcion \"(x-1)/4-(x-5)/36\" es: %f\n", resultado);  /* se imprime el resultado en pantalla de la primer funcion */
+	printf("el valor de la segunda funcion \"6((x+1)/8-(2x-3)/16)\" es: %f\n", resultadoDos);  /* se imprime el resultado en pantalla de la segunda funcion */
 
 	 
 	
