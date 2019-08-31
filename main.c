be8362d9f267d9ef71235580cbcf9d22c2376077
Luis Diaz <99diazluisfernand@gmail.com>
2019-08-31 10:04:22 -0500

    Actividad 1

--- main.c
+++ main.c
@@ -1,98 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Luis Fenrnado Flores Diaz */
+/* 
+Autor: Luis Fernando Flores Diaz
+Fecha: 25/08/2019
+Grupo: 008
+Horario: Sabado 9:00am - 11:00am
+Matricula: 1821936
+ */
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-int main(int argc, char *argv[]) {
-=======
-int main() 
-{ 
-<<<<<<< HEAD
- /* Actividad 1*/
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	float x, resultado, resultadoDos, resultadoTres, resultadoCuatro;  /* se declara el resultado y la variable "x" como tipo flotante en caso de que se encuentre un entero con decimal como respuesta de la funcion*/ 
-=======
-int main() 
-{ 
- /* Actividad 1*/
-	float x, resultado, resultadoDos, resultadoTres;  /* se declara el resultado y la variable "x" como tipo flotante en caso de que se encuentre un entero con decimal como respuesta de la funcion*/ 
->>>>>>> 59b1c6abc2bcbad18b48a2bbc96c3642cd8123d6
-=======
-	float x, resultado, resultadoDos, resultadoTres;  /* se declara el resultado y la variable "x" como tipo flotante en caso de que se encuentre un entero con decimal como respuesta de la funcion*/ 
->>>>>>> parent of 83d9542... Cuarta Funcion Terminada
-=======
-	float x, resultado, resultadoDos, resultadoTres;  /* se declara el resultado y la variable "x" como tipo flotante en caso de que se encuentre un entero con decimal como respuesta de la funcion*/ 
->>>>>>> parent of 83d9542... Cuarta Funcion Terminada
-							 /* se agregan las variables necesarias para los 3 resultados*/
-=======
-	float x, resultado, resultadoDos;  /* se declara el resultado y la variable "x" como tipo flotante en caso de que se encuentre un entero con decimal como respuesta de la funcion*/ 
-							 /* se agrega una tercer variable para el segundo resultado respectivo a la segunda funcion algebraica*/
->>>>>>> parent of 00bd864... Tercer Funcion Terminada
-	printf("indique el valor de x para las funciones \n");  /* se le indica al us teclear el valor de x para la funcion mencionada*/
-	scanf("%f", &x); 		 
-	resultado=(x-1)/4-(x-5)/36;   /* se le asigna el valor de la funcion a la variable correspondiente*/
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+int main() {
+	/*****Actividad 1*****/
+	int numeroUno, numeroDos;
 	
-=======
->>>>>>> 59b1c6abc2bcbad18b48a2bbc96c3642cd8123d6
-=======
->>>>>>> parent of 83d9542... Cuarta Funcion Terminada
-=======
->>>>>>> parent of 83d9542... Cuarta Funcion Terminada
-	resultadoDos = 6*((x+1)/8-(2*x-3)/16);		/* se le asigna el valor de la funcion a la variable correspondiente*/
+	printf("indroduzca el primer numero\n"); /* se solicita al usuario los numeros a validar*/
+	scanf("%d", &numeroUno);
+	printf("introduzca el desgundo numero\n");
+	scanf("%d", &numeroDos);
 	
-	printf("el valor de la primera funcion \"(x-1)/4-(x-5)/36\" es: %f\n", resultado);  /* se imprime el resultado en pantalla de la primer funcion */
-	printf("el valor de la segunda funcion \"6((x+1)/8-(2x-3)/16)\" es: %f\n", resultadoDos);  /* se imprime el resultado en pantalla de la segunda funcion */
-	
-	 /* Actividad 2*/
-<<<<<<< HEAD
-	resultadoTres= (x-2)/3;					 /* se ordena las operaciones por jerarquia para el valor correcto de la funcion*/
-	resultadoTres= 1-resultadoTres;
-	resultadoTres= x-resultadoTres;
-	resultadoTres= (.66666667)*resultadoTres+1;
-	
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	resultadoCuatro= 2-(-2*(x+1)-(x-3)/2);		/* esta es la cuarta funcion*/
-	
-	printf("el valor de la tercera funcion \"(2/3)*(x-(1-(x-2)/3))+1\" es: %f\n", resultadoTres);  /* se imprime el resultado en pantalla de la tercera funcion */
-	printf("el valor de la cuarta funcion \"2-(-2*(x+1)-(x-3)/2)\" es: %f\n", resultadoCuatro);  /* se imprime el resultado en pantalla de la cuarta funcion */
-=======
-	printf("el valor de la tercera funcion \"(2/3)*(x-(1-(x-2)/3))+1\" es: %f\n", resultadoTres);  /* se imprime el resultado en pantalla de la tercera funcion */
->>>>>>> 59b1c6abc2bcbad18b48a2bbc96c3642cd8123d6
-=======
-	printf("el valor de la tercera funcion \"(2/3)*(x-(1-(x-2)/3))+1\" es: %f\n", resultadoTres);  /* se imprime el resultado en pantalla de la tercera funcion */
->>>>>>> parent of 83d9542... Cuarta Funcion Terminada
-=======
-	printf("el valor de la tercera funcion \"(2/3)*(x-(1-(x-2)/3))+1\" es: %f\n", resultadoTres);  /* se imprime el resultado en pantalla de la tercera funcion */
->>>>>>> parent of 83d9542... Cuarta Funcion Terminada
-	 
-=======
->>>>>>> parent of 00bd864... Tercer Funcion Terminada
-	 
-=======
-	float x, resultado;  /* se declara el resultado y la variable "x" como tipo flotante en caso de que se encuentre un entero con decimal como respuesta de la funcion*/
-	printf("indique el valor de x para la funcion: \"(x-1)/4-(x-5)/36\"\n");  /* se le indica al us teclear el valor de x para la funcion mencionada*/
-	scanf("%f", &x); 		 
-	resultado=(x-1)/4-(x-5)/36;  
-	printf("el valor de la primera funcion es: %f\n", resultado);  /* se imprime el resultado en pantalla */
->>>>>>> parent of bea7938... Segunda Funcion Terminada
-	
-	
-<<<<<<< HEAD
->>>>>>> parent of 59b1c6a... Revert "Cuarta Funcion Terminada"
-=======
->>>>>>> 59b1c6abc2bcbad18b48a2bbc96c3642cd8123d6
-=======
-int main(int argc, char *argv[]) {
->>>>>>> parent of acd06cd... Primer Funcion terminada
+	if (numeroUno==numeroDos){
+		printf("los dos numeros son iguales"); /* en este caso se campararan, en caso cierto se le indicara que son iguales*/
+	}
+	else if (numeroUno<numeroDos){
+		printf("el numero %d, es mas grande que %d\n", numeroDos, numeroUno);
+	}
+	else if (numeroUno>numeroDos){
+		printf("el numero %d, es mas grande que %d\n", numeroUno, numeroDos);
+	}
 	return 0;
 }
